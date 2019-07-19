88f6b1a3c7997ef9c83a7cfd9bb8874b794e093c
Antonio García-Domínguez <nyoescape@gmail.com>
2019-07-19 06:59:25 +0200

    check_results.R: relax comparison to checking whether inconsistencies are found at all

--- reporting/check_results.R
+++ reporting/check_results.R
@@ -9,23 +9,39 @@ for (tool in tools) {
   for (mutant_set in mutant_sets) {
     mset_data = subset(tool_data, MutantSet==mutant_set)
     sources = unique(mset_data$Source)
-    for (source in sources) {
-        source_data = subset(mset_data, Source==source)
-        source_n = length(row.names(source_data))
+    for (msource in sources) {
+      source_data = subset(mset_data, Source==msource)
+      source_n = length(row.names(source_data))
 
-        for (i in 1:source_n) {
-            query.row = source_data[i,]
-            expected.row = subset(expected, MutantSet==query.row$MutantSet & Source==query.row$Source & Mutant==query.row$Mutant)
+      for (i in 1:source_n) {
+        query.row = source_data[i,]
 
-            if (length(as.character(expected.row$MetricValue)) > 0) {
-               if (as.character(query.row$MetricValue) != as.character(expected.row$MetricValue)) {
-                  print(paste(tool, "is wrong. Was ", query.row$MetricValue, "but expected", expected.row$MetricValue, "for mutant set", mutant_set, "source", source, "mutant", query.row$Mutant, "run", query.row$RunIndex))
-               }
-            } else {
-              print(paste("Warning:", tool, "produced the result", query.row$MetricValue, "but expected result is unavailable for mutant set", mutant_set, "source", source, "mutant", query.row$Mutant, "run", query.row$RunIndex))
-            }
+        # By using as.character in Source, we don't need to have run all sources to run the comparison
+        expected.row = subset(expected,
+                              MutantSet == query.row$MutantSet
+                              & as.character(Source)==as.character(query.row$Source)
+                              & Mutant==query.row$Mutant)
 
+        if (length(as.character(expected.row$MetricValue)) > 0) {
+          actual_consistent = ifelse(query.row$MetricValue == 0, "consistent", "not consistent");
+          expected_consistent = ifelse(expected.row$MetricValue == 0, "consistent", "not consistent");
+          if (actual_consistent != expected_consistent) {
+            print(paste(tool, "is wrong. Was",
+                        actual_consistent, "(", query.row$MetricValue, ") but expected",
+                        expected_consistent, "(", expected.row$MetricValue, ") for mutant set",
+                        mutant_set, "source", msource,
+                        "mutant", query.row$Mutant,
+                        "run", query.row$RunIndex))
+          }
+        } else {
+          print(paste("Warning:", tool, "produced the result", actual_consistent,
+                      "(", query.row$MetricValue, ")",
+                      "but expected result is unavailable for mutant set", mutant_set,
+                      "source", msource, "mutant", query.row$Mutant,
+                      "run", query.row$RunIndex))
         }
+
+      }
     }
 
   }
